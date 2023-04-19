8742608fc2e8deadcc84cbdfbf434148ee9a6d9e
Delhibabu1 <sanjayp2209@gmail.com>
2023-04-19 06:43:38 +0530

    PHP

--- helloworld.php
+++ helloworld.php
@@ -1,17 +1,3 @@
 <?php
-
-/**
- * Created by PhpStorm.
- * User: Hollyphat
- * Date: 10/7/20
- * Time: 8:38 AM
- */
- 
-function greet(){
-
- echo "Hello world";
- 
- }
- 
- 
- greet();
+echo 'Hello, World!';
+?>
